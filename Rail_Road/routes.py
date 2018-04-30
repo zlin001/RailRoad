3075a4a4f962c6094d24bb00eff6e98cd515eabf
LiuYuHao <liuyuhao717@gmail.com>
2018-04-30 16:30:28 -0400

    adding documentation

--- Rail_Road/routes.py
+++ Rail_Road/routes.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from . import app
 
+# This is exampel for how to make a route for different page
 @app.route("/")
 def index():
     return render_template("index.html")
