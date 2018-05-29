f3d61696e11b90dc428ba8de0ba07bc178bb6044
zlin001 <zlin001@citymail.cuny.edu>
2018-05-29 01:18:10 -0400

    update

--- application.py
+++ application.py
@@ -1,5 +1,4 @@
 from Rail_Road import app as application
 
 if __name__ == "__main__":
-    application.debug = False
-    application.run()
+    application.run(host='127.0.0.1', port=8080, debug=True)
